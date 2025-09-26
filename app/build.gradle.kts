6a2b2804d9908b7f973cbf980f639730e30a8aeb
yus <swiftcase@gmail.com>
2025-09-26 17:21:32 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,4 +26,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    
+    // Add this for SVG support:
+    implementation("com.caverock:androidsvg:1.4")
 }
