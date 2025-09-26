301f7d277392a3a86407692238bb46f7ecd9e3f3
yus <swiftcase@gmail.com>
2025-09-26 16:14:02 +0800

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 34
+    
+    defaultConfig {
+        applicationId = "com.example.mysvgeditor"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
