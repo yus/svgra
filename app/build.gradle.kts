819f6d0a2228adee9c2964c456de43392fcf5fd2
yus <swiftcase@gmail.com>
2025-09-26 16:17:40 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 34
     
     defaultConfig {
-        applicationId = "com.example.mysvgeditor"
+        applicationId = "com.example.svgra"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
